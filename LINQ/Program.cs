4ba64cf8f79ca973bd053fc1cc60ddfb791dedc5
Javian Phillips <javianph3@gmail.com>
2025-08-10 20:17:25 -0500

    Ordered game titles by length

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() {"God of War", "Assassin's Creed", "Call of Duty", "Bully"};
+            var games = videoGames.OrderBy(g => g.Length);
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
